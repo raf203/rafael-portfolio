846d99f6100bd39b92d7f93afa49d9e50e12c045
Rafael Cardoso <rrafa203@gmail.com>
2022-02-10 18:42:08 -0800

    add header and vav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import "./App.css";
 import About from "./components/About";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+import ContactForm from "./components/Contact";
+
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <About />
+      <Portfolio></Portfolio>
+      <ContactForm></ContactForm>
+      <Resume></Resume>
+      <Footer></Footer>
     </div>
   );
 }
